29a5187b70e04857265c95f7f2f88920c0d6314b
Christine Greaney-Kelley <kelleyc416@gmail.com>
2022-05-18 13:33:57 +0000

    Complete part two of prev state lesson

--- my-app/src/components/StatefulGreetingWithCallbackPrevState.js
+++ my-app/src/components/StatefulGreetingWithCallbackPrevState.js
@@ -13,29 +13,55 @@ class StatefulGreetingWithCallbackPrevState extends Component {
         };
     }
 
+    // handleClick() {
+    //     this.setState({
+    //             introduction: this.state.introduction === 'Hello' ?
+    //                 'Goodbye' : 'Hello',
+    //             buttonText: this.state.buttonText === 'Exit' ?
+    //                 'Enter' : 'Exit',
+
+    //         }, () => {
+    //             console.log('new state', this.state.introduction)
+    //             console.log('new state', this.state.buttonText)
+    //         }
+
+    //     );
+    //     console.log(this.state.introduction);
+    //     console.log(this.state.buttonText);
+    // }
+
+
+
+    // increment() {
+    //     this.setState({
+    //         count: this.state.count + 1
+    //     });
+    //     console.log(this.state.count)
+    // }
+
     handleClick() {
-        this.setState({
-                introduction: this.state.introduction === 'Hello' ?
-                    'Goodbye' : 'Hello',
-                buttonText: this.state.buttonText === 'Exit' ?
-                    'Enter' : 'Exit',
-
-            }, () => {
-                console.log('new state', this.state.introduction)
-                console.log('new state', this.state.buttonText)
-            }
-
-        );
-        console.log(this.state.introduction);
-        console.log(this.state.buttonText);
+        this.setState((prevState, prevProps) => {
+            console.log('Previous state', prevState);
+            console.log('Previous props', prevProps);
+            return {
+                introduction: prevState.introduction === 'Hello' ? 'Goodbye' : 'Hello',
+                buttonText: prevState.buttonText === 'Exit' ? 'Enter' : 'Exit',
+            };
+        });
     }
+
 
     increment() {
-        this.setState({
-            count: this.state.count + 1
+        this.setState((prevState, prevProps) => {
+            console.log('Previous state', prevState);
+            console.log('Previous props', prevProps);
+            return {
+                count: prevState.count + 1
+            };
         });
-        console.log(this.state.count)
     }
+
+
     render() {
         return ( <
             div >
