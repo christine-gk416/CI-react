c54efb7a9241238aac04148d0ced51f95b70e9f1
Christine Greaney-Kelley <kelleyc416@gmail.com>
2022-05-18 09:38:09 +0000

    Create functional greeting

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import FunctionalGreeting from './components/FunctionalGreeting'
+
+function App() {
+    return ( <
+        div className = "App" >
+
+        <
+        FunctionalGreeting / > < /
+        div >
+    );
+
+
+}
+
+export default App;
