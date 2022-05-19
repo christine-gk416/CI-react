3d3ac8fa945e5e0e4b7ba3980d4c08edc41f6a91
Christine Greaney-Kelley <kelleyc416@gmail.com>
2022-05-19 09:25:09 +0000

    ES7 snippets lesson

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function ES7Snippets() {
+    return ( <
+        div > ES7Snippets < /div>
+    )
+}
+
+export class ES7Snippets extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            first
+        }
+    }
+    console.log(first)
+    console.log('first', first)
+    render() {
+        return ( <
+            div > ES7Snippets < /div>
+        )
+    }
+}
+
+export default ES7Snippets
